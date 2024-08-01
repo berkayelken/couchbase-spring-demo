c94aeed3cd38c3c2a3dbe8175392354d4dd41ad5
Berkay Yelken <berkayelken@gmail.com>
2024-08-01 19:30:17 +0300

    Create CouchbaseConnectionConfiguration.java

---
+++ src/main/java/io/github/berkayelken/couchbase/spring/demo/configuration/CouchbaseConnectionConfiguration.java
@@ -0,0 +1,41 @@
+package io.github.berkayelken.couchbase.spring.demo.configuration;
+
+import io.github.berkayelken.couchbase.spring.demo.domain.command.CustomerEvent;
+import io.github.berkayelken.couchbase.spring.demo.domain.query.Customer;
+import io.github.berkayelken.couchbase.spring.demo.properties.CouchbaseConnectionProperties;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
+import org.springframework.data.couchbase.repository.config.RepositoryOperationsMapping;
+
+@Getter
+@Setter
+@Configuration
+@EnableCouchbaseRepositories ("io.github.berkayelken.couchbase.spring.demo.repository")
+public class CouchbaseConnectionConfiguration extends AbstractCouchbaseConfiguration {
+	private String connectionString;
+	private String userName;
+	private String password;
+	private String bucketName;
+
+	@Getter (AccessLevel.NONE)
+	private final CouchbaseConnectionProperties properties;
+
+	@Autowired
+	public CouchbaseConnectionConfiguration(CouchbaseConnectionProperties properties) {
+		this.properties = properties;
+	}
+
+	@SneakyThrows
+	@Override
+	public void configureRepositoryOperationsMapping(RepositoryOperationsMapping baseMapping) {
+		baseMapping.mapEntity(Customer.class, properties.createQueryConnection());
+		baseMapping.mapEntity(CustomerEvent.class, properties.createCommandConnection());
+	}
+
+}
