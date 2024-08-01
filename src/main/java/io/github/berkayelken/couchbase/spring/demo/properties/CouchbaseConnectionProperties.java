996174f81f4b7562a0a0a69954f5165354f7e7a6
Berkay Yelken <berkayelken@gmail.com>
2024-08-01 18:55:56 +0300

    Create CouchbaseConnectionProperties.java

---
+++ src/main/java/io/github/berkayelken/couchbase/spring/demo/properties/CouchbaseConnectionProperties.java
@@ -0,0 +1,29 @@
+package io.github.berkayelken.couchbase.spring.demo.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.core.CouchbaseTemplate;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties("couchbase-connection")
+public class CouchbaseConnectionProperties {
+	private CouchbaseConnectionProperty commandSource;
+	private CouchbaseConnectionProperty querySource;
+	private CouchbaseConnectionProperty cachingSource;
+
+	public CouchbaseTemplate createCommandConnection() {
+		return commandSource.createTemplate();
+	}
+
+	public CouchbaseTemplate createQueryConnection() {
+		return querySource.createTemplate();
+	}
+
+	public CouchbaseTemplate createCachingConnection() {
+		return cachingSource.createTemplate();
+	}
+}
