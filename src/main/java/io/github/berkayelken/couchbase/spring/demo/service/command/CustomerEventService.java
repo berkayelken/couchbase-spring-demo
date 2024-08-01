1047571807b6e7802bf684c849f5615aff728ec4
Berkay Yelken <berkayelken@gmail.com>
2024-08-01 20:34:43 +0300

    Create CustomerEventService.java

---
+++ src/main/java/io/github/berkayelken/couchbase/spring/demo/service/command/CustomerEventService.java
@@ -0,0 +1,40 @@
+package io.github.berkayelken.couchbase.spring.demo.service.command;
+
+import io.github.berkayelken.couchbase.spring.demo.domain.command.EventOperation;
+import io.github.berkayelken.couchbase.spring.demo.domain.command.request.EventRequest;
+import io.github.berkayelken.couchbase.spring.demo.domain.query.Customer;
+import io.github.berkayelken.couchbase.spring.demo.repository.CommandRepository;
+import io.github.berkayelken.couchbase.spring.demo.service.query.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerEventService {
+	private final CommandRepository repository;
+	private final CustomerService customerService;
+
+	@Autowired
+	public CustomerEventService(CommandRepository repository, CustomerService customerService) {
+		this.repository = repository;
+		this.customerService = customerService;
+	}
+
+	public Customer createCustomer(EventRequest request) {
+		request.setOperation(EventOperation.CREATE);
+		repository.saveAll(request.convertCustomerEvents());
+		return customerService.getCustomer(request.getRelatedCustomer());
+	}
+
+	public Customer updateCustomer(EventRequest request) {
+		request.setOperation(EventOperation.UPDATE);
+		repository.saveAll(request.convertCustomerEvents());
+		return customerService.getCustomer(request.getRelatedCustomer());
+	}
+
+	public void deleteCustomer(EventRequest request) {
+		request.setOperation(EventOperation.DELETE);
+		repository.saveAll(request.convertCustomerEvents());
+	}
+
+
+}
