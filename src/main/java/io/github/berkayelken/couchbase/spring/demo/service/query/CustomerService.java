c348543eda8efcd6c5326e886df1039bdc841946
Berkay Yelken <berkayelken@gmail.com>
2024-08-01 20:06:59 +0300

    Create CustomerService.java

---
+++ src/main/java/io/github/berkayelken/couchbase/spring/demo/service/query/CustomerService.java
@@ -0,0 +1,27 @@
+package io.github.berkayelken.couchbase.spring.demo.service.query;
+
+import io.github.berkayelken.couchbase.spring.demo.domain.query.Customer;
+import io.github.berkayelken.couchbase.spring.demo.exception.RecordNotFoundException;
+import io.github.berkayelken.couchbase.spring.demo.repository.QueryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+	private final QueryRepository repository;
+
+	@Autowired
+	public CustomerService(QueryRepository repository) {
+		this.repository = repository;
+	}
+
+	public Customer getCustomer(String id) {
+		return repository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
+	}
+
+	public List<Customer> getAllCustomers() {
+		return repository.findAll();
+	}
+}
