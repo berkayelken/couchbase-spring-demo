608d70748877c776abcc9412069f6da6a48b501f
Berkay Yelken <berkayelken@gmail.com>
2024-08-01 18:22:45 +0300

    Create EventFieldRequestTest.java

---
+++ src/test/java/io/github/berkayelken/couchbase/spring/demo/domain/command/request/EventFieldRequestTest.java
@@ -0,0 +1,61 @@
+package io.github.berkayelken.couchbase.spring.demo.domain.command.request;
+
+import io.github.berkayelken.couchbase.spring.demo.domain.command.EventOperation;
+import io.github.berkayelken.couchbase.spring.demo.exception.EventException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EventFieldRequestTest {
+	private static final String TEST_STR = "TEST_STR";
+
+	@Test
+	public void testNoArgsConstructor() {
+		Assertions.assertDoesNotThrow(EventFieldRequest::new);
+	}
+
+	@Test
+	public void testAccessors() {
+		EventFieldRequest request = new EventFieldRequest();
+
+		Assertions.assertNull(request.getField());
+		Assertions.assertNull(request.getValue());
+	}
+
+	@Test
+	public void testMutators() {
+		EventFieldRequest request = new EventFieldRequest();
+
+		request.setField(EventField.NAME);
+		request.setValue(TEST_STR);
+
+		Assertions.assertEquals(EventField.NAME, request.getField());
+		Assertions.assertEquals(TEST_STR, request.getValue());
+	}
+
+	@Test
+	public void testCheckValueTypeFeasible() {
+		EventFieldRequest request = new EventFieldRequest();
+
+		Assertions.assertFalse(request.checkValueTypeFeasible());
+
+		request.setField(EventField.NAME);
+		Assertions.assertFalse(request.checkValueTypeFeasible());
+
+		request.setValue(Integer.MAX_VALUE);
+		Assertions.assertFalse(request.checkValueTypeFeasible());
+
+		request.setValue(TEST_STR);
+		Assertions.assertTrue(request.checkValueTypeFeasible());
+	}
+
+	@Test
+	public void testConvertCustomerEvent() {
+		EventFieldRequest request = new EventFieldRequest();
+
+		Assertions.assertThrows(EventException.class, () -> request.convertCustomerEvent(TEST_STR, EventOperation.UPDATE));
+
+		request.setField(EventField.NAME);
+		request.setValue(TEST_STR);
+		Assertions.assertDoesNotThrow(() -> request.convertCustomerEvent(TEST_STR, EventOperation.UPDATE));
+	}
+}
